75ad47381d6cf393d005fa6fe060730e0585d534
Miniontoby <tobias.gaarenstroom@gmail.com>
2025-10-10 11:36:28 +0200

    Update uc12 - Productcategoriën toevoegen
    Changes:
    - Maak class Category
    - Maak class ProductCategory
    - Maak Interface en Repository voor Category
    - Maak Interface en Repository voor ProductCategory
    - Maak Interface en Service voor Category
    - Maak Interface en Service voor ProductCategory
    - Registreer de gemaakte Repo's en services in MauiProgram
    - Maak CategoriesViewModel.
    - Maak CategoriesView.
    - Registreer De view en het ViewModel in MauiProgram.
    - Maak een menu entry in de tabbar in AppShell.xaml en registreer route in AppShell.xaml.cs
    - Maak ProductCategoriesViewModel.
    - Maak ProductCategoriesView.
    - Registreer De view en het ViewModel in MauiProgram.
    - Registreer route naar ProductCategoriesView in AppShell.xaml.cs
    
    Not working:
    - Zorg dat de ProductCategoriesView gestart kan worden na het klikken op een Category in CategoriesView

---
+++ Grocery.App/ViewModels/ProductCategoriesViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using System.Collections.ObjectModel;
+
+namespace Grocery.App.ViewModels
+{
+    [QueryProperty(nameof(Category), nameof(Category))]
+    public partial class ProductCategoriesViewModel : BaseViewModel
+    {
+        private readonly IProductCategoryService _productCategoryService;
+
+        public ObservableCollection<ProductCategory> MyProductCategories { get; set; } = [];
+
+        [ObservableProperty]
+        Category category = new(0, "");
+
+        public ProductCategoriesViewModel(IProductCategoryService productService)
+        {
+            _productCategoryService = productService;
+            if (category.Id == 0) Category = new(3, "Zuivel");
+            Load(category.Id);
+        }
+
+        private void Load(int id)
+        {
+            MyProductCategories.Clear();
+            foreach (var item in _productCategoryService.GetAllOnCategoryId(id)) MyProductCategories.Add(item);
+        }
+    }
+}
